f856a41b96b798df5db826619d46f64efd8f79ef
Manuel Spigolon <behemoth89@gmail.com>
2021-05-12 00:12:15 +0200

    docs: clearer steps

--- Chapter 1/index.mjs
+++ Chapter 1/index.mjs
@@ -1,4 +1,9 @@
-import fastify from 'fastify'
-
-const server = fastify()
-server.listen(8080)
+import fastify from 'fastify' // 1: import the module in your project
+const serverOptions = { // 2: define some settings for the server
+  logger: true
+}
+const server = fastify(serverOptions) // 3: instantiate the server
+await server.listen({ // 4: start the server
+  port: 8080,
+  host: '0.0.0.0'
+}) 
