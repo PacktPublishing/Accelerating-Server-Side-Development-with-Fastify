360fc1e19b3cda671de3f8e243a03864e4cb7726
Manuel Spigolon <manuel.spigolon@nearform.com>
2022-07-02 16:49:16 +0200

    chapter-9: bump to fastify v4

--- Chapter 9/plugins/cors.js
+++ Chapter 9/plugins/cors.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const fastifyCors = require('fastify-cors')
+const fastifyCors = require('@fastify/cors')
 
 module.exports = fp(async function corsPlugin (fastify, opts) {
   fastify.register(fastifyCors, {
--- Chapter 9/plugins/sensible.js
+++ Chapter 9/plugins/sensible.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const fastifySensible = require('fastify-sensible')
+const fastifySensible = require('@fastify/sensible')
 
 module.exports = fp(async function sensiblePlugin (fastify, opts) {
   fastify.register(fastifySensible, {
