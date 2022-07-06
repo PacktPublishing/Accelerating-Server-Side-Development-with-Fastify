084e69b3bd2d6e6a40be0ab499e855f66601eb68
Manuel Spigolon <manuel.spigolon@nearform.com>
2022-07-06 07:16:12 +0200

    chapter-9: bump to fastify v4 (#13)
    * chapter-9: bump to fastify v4
    
    * fix: commit package-lock

--- Chapter 9/app.js
+++ Chapter 9/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const AutoLoad = require('@fastify/autoload')
 
 module.exports = async function (fastify, opts) {
   fastify.register(AutoLoad, {
--- Chapter 9/plugins/mongo-data-source.js
+++ Chapter 9/plugins/mongo-data-source.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const fp = require('fastify-plugin')
-const fastifyMongo = require('fastify-mongodb')
+const fastifyMongo = require('@fastify/mongodb')
 
 module.exports = fp(async function datasourcePlugin (fastify, opts) {
   fastify.register(fastifyMongo, {
