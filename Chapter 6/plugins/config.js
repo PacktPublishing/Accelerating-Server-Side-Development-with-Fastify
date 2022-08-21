1a4ec85b34b38943baeb9bfdc5aeefcd1233578a
Kyriakos Markakis <11089475+marquicodes@users.noreply.github.com>
2022-08-21 12:35:30 +0300

    fix: Chapter 6 review (#18)
    * doc: Update node version
    
    * doc: Fix typo
    
    * refactor: Remove data key as it is not used yet
    
    * chore: Change port to 3000 as it is in the book
    
    Co-authored-by: Kyriakos Markakis <>

--- Chapter 6/plugins/config.js
+++ Chapter 6/plugins/config.js
@@ -6,7 +6,6 @@ const fastifyEnv = require('@fastify/env')
 module.exports = fp(async function configLoader (fastify, opts) {
   await fastify.register(fastifyEnv, {
     confKey: 'secrets',
-    data: opts.configData,
     schema: fastify.getSchema('schema:dotenv')
   })
 },
