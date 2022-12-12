17503181af441278ab6536507f2f4a5d413e925e
Kyriakos Markakis <>
2022-12-12 21:09:16 +0200

    fix: Updates column name on script to match table

--- Chapter 14/app-without-dataloaders.js
+++ Chapter 14/app-without-dataloaders.js
@@ -29,7 +29,7 @@ async function run () {
     },
     Mutation: {
       changeNickName: async function changeNickNameFunc (parent, args, context, info) {
-        const sql = SQL`UPDATE person SET nickName = ${args.nickName} WHERE id = ${args.personId}`
+        const sql = SQL`UPDATE person SET nick = ${args.nickName} WHERE id = ${args.personId}`
         const { changes } = await context.app.sqlite.run(sql)
         if (changes === 0) {
           throw new Error(`Person id ${args.personId} not found`)
@@ -39,7 +39,7 @@ async function run () {
         return person
       },
       changeNickNameWithInput: async function changeNickNameFunc (parent, { input }, context, info) {
-        const sql = SQL`UPDATE person SET nickName = ${input.nick} WHERE id = ${input.personId}`
+        const sql = SQL`UPDATE person SET nick = ${input.nick} WHERE id = ${input.personId}`
         const { changes } = await context.app.sqlite.run(sql)
         if (changes === 0) {
           throw new Error(`Person id ${input.personId} not found`)
