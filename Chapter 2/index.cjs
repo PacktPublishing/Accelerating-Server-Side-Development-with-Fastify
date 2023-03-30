64bc7ab8611bb46dc3d0b33a65218bf92304cea4
Kyriakos Markakis <>
2023-03-30 22:32:10 +0300

    chore: Change logs to match output format on the book

--- Chapter 2/index.cjs
+++ Chapter 2/index.cjs
@@ -1,7 +1,7 @@
 const Fastify = require('fastify')
 const app = Fastify({ logger: true }) // [1]
 app.register(async function (fastify, opts) { // [2]
-  console.log('Registering my first plugin.')
+  app.log.info('Registering my first plugin.')
 })
 app.ready() // [3]
   .then(() => {
