ed4ce4ae5e2dc98595d6111ca3b09cb8fa5bc093
Kyriakos Markakis <>
2023-04-02 04:14:12 +0300

    fix: Load schema to validate todo update requests

--- Chapter 7/routes/todos/schemas/loader.js
+++ Chapter 7/routes/todos/schemas/loader.js
@@ -10,4 +10,5 @@ module.exports = fp(async function schemaLoaderPlugin (fastify, opts) {
   fastify.addSchema(require('./create-response.json'))
   fastify.addSchema(require('./read-params.json'))
   fastify.addSchema(require('./status-params.json'))
+  fastify.addSchema(require('./update-body.json'))
 })
